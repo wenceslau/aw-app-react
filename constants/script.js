d97f0ef4d0790a19b3f8d6bf024392b6a4c1ba38
Wenceslau Neto <wbaneto@gmail.com>
2024-06-16 11:14:13 +0100

    feat: programming logic

--- constants/script.js
+++
@@ -1,3 +0,0 @@
-const value1 = 1;
-
-console.log('value1 = ', value1);
--- objects/script.js
+++
@@ -1,8 +0,0 @@
-const user = {
-    id: 1,
-    name: 'Siri',
-    birthdate: '4 de outubro de 2011',
-    isHuman: false
-};
-
-console.log(user);
